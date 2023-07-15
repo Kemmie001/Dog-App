b4608fe7f75f789e197521c9d7f2fa3946f2d176
Kemmie001 <oluwakemisola757@gmail.com>
2023-07-15 15:16:21 +0100

    feat: defined color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#CC8F56",
+        secondary: "#51e1a7",
+        light: "#FDFDFD",
+        dark: "#333333",
+        "dark-light": "#323259",
+        neutral: "#828282",
+        "neutral-border": "#DFDFDF",
+        "neutral-bg": "#F2F2F2",
+      },
+    },
   },
   plugins: [],
-}
-
+};
