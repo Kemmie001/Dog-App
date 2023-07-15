9b36ad6e92362ae5c8e2073fa5ced87df63a068b
Kemmie001 <oluwakemisola757@gmail.com>
2023-07-15 15:17:46 +0100

    feat: navigation bar and the website logo

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+<template>
+  <div
+    class="bg-white fixed w-full shadow-sm py-4 px-5 md:px-10 flex justify-between items-center"
+  >
+    <div class="flex gap-5 items-center">
+      <div class="">
+        <img class="w-8 h-8" src="../assets/logo.svg" alt="" />
+      </div>
+      <h6 class="font-bold text-dark text-lg">DOGGIES</h6>
+    </div>
+    <div class="flex gap-4 items-center">
+      <!-- <input
+        class="border-neutral border rounded-md pl-2 py-2 outline-none"
+        placeholder="Search"
+        type="search"
+        name=""
+        id=""
+      /> -->
+      <a href="" class="px-4 py-2 text-white bg-primary rounded-md">Github</a>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
