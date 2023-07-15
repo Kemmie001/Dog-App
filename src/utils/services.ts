e751d7380122b8f539ae58a3cd9dde083b90fe69
Kemmie001 <oluwakemisola757@gmail.com>
2023-07-15 15:19:23 +0100

    feat: added api endpoints and  axios

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -1,14 +1,14 @@
-import { http } from "./http";
+import { http } from "./https";
 
 export function getAllBreeds() {
   return http.get("breeds/list/all");
 }
-export function getRandomDogImages(amount) {
+export function getRandomDogImages(amount: any) {
   return http.get(`breeds/image/random/${amount}`);
 }
-export function getDogImagesByBreed(breed, amount) {
+export function getDogImagesByBreed(breed: any, amount: any) {
   return http.get(`/breed/${breed}/images`);
 }
-export function subBreedImages(breed, sub, amount) {
+export function subBreedImages(breed: any, sub: any, amount: any) {
   return http.get(`/breed/${breed}/${sub}/images/random/${3}`);
 }
