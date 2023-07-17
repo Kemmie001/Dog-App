b56315a956b2dee0426d60f66078b991a8d062f5
Kemmie001 <oluwakemisola757@gmail.com>
2023-07-17 01:42:29 +0100

    fix: added correct routes to the logo and button

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,23 +3,16 @@
   <div
     class="bg-white fixed z-10 w-full shadow-sm py-4 px-4 md:px-10 flex justify-between items-center"
   >
-    <div class="flex gap-5 items-center">
+    <router-link to="/" class="flex gap-5 items-center">
       <div class="">
         <img class="w-8 h-8" src="../assets/logo.svg" alt="" />
       </div>
       <h6 class="font-bold text-dark text-lg">DOGGIES</h6>
-    </div>
+    </router-link>
     <div class="flex gap-4 items-center">
-      <!-- <input
-        class="border-neutral border rounded-md pl-2 py-2 outline-none"
-        placeholder="Search"
-        type="search"
-        name=""
-        id=""
-      /> -->
       <a
         href="https://github.com/Kemmie001/Dog-App"
-        target="_blank"
+        target=""
         class="px-4 py-2 text-white bg-primary rounded-md"
         >Github</a
       >
