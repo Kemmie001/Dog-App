d15581a1ea298361815a5dcad9d0f5f883c6c5d0
Kemmie001 <oluwakemisola757@gmail.com>
2023-07-17 01:55:41 +0100

    feat: added dog infomation page route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/error",
+      name: "error",
+      component: () => import("../views/ErrorView.vue"),
+    },
+    {
+      path: "/dog/:slug/:slug_id",
+      name: "dog",
       // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
+      // this generates a separate chunk (Info.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import("../views/DogInfo/_breed/_InfoView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
